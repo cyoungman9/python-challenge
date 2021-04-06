a0538d3bc9279549b4e79631f2c16024b6a7f274
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-04-06 11:11:48 -0400

    Adding New Files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-bank_csv = os.path.join("..", "Resources", "budget_data.csv")
+bank_csv = os.path.join("Resources", "budget_data.csv")
 
 # lists for csv file values
 months = []
@@ -9,56 +9,52 @@ profit = 0
 profit_change = []
 var = list()
 count = 0
+dif_profit = 0
+prev_profit = 0
+max_profit = 0
+min_profit = 999999
+max_date = ""
+min_date = ""
 
-def print_percentage(bank_data):
-    months = int(bank_data[0])
-    profits = int(bank_data[1])
-
-
-
+# def print_percentage(bank_data):
+#     months = int(bank_data[0])
+#     profits = int(bank_data[1])
 
 with open(bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader) 
 
-    Print("Financial Analysis")
-    Print("----------------------------")  
+    print("Financial Analysis")
+    print("----------------------------")  
 
 #read through the data
     for row in csvreader:
         count = count + 1
         profit = profit +int(row[1])
-        #months.append = (row[0])
-        #profit.append = (int(row[1]))
+        dif_profit = int(row[1]) - prev_profit
+        profit_change.append(dif_profit)
+        prev_profit = int(row[1])
+        if dif_profit > max_profit: 
+            max_profit = dif_profit
+            max_date = row[0]
 
+        if dif_profit < min_profit: 
+            min_profit = dif_profit
+            min_date = row[0]
 
-#total_months = len(months)   
-#print(f"Month Count: {months}")  
+    avg_profit= sum(profit_change[1:])/ len(profit_change[1:])
 
-
-#for row in range(len(month_list)):
-#month_count == row[0] 
-
-
-
-# max_change = max(profit_change)
-# min_change = min(profit_change)
-
-# if min_change == row[0]:
-#     print_percentages(row)
-
-# if max_change == row[0]:
-#     print_percentages(row)
 
 # import sys
 # original_stdout = sys.stdout
 
 
 
-Print(f"Total Months: {months}")
-Print(f"Total: {total_amount}")
-Print(f"Average Change: {}")
-Print(f"Greatest Increase in Profits: {} + {()}")
-Print(f"Greatest Decrease in Profits: {} + {()}")
+print(f"Total Months: {count}")
+print(f"Total: {profit}")
+# print(profit_change)
+print(f"Average Change:{avg_profit:.2f}")
+print(f"Greatest Increase in Profits: {max_date} + ${(max_profit)}")
+print(f"Greatest Decrease in Profits: {min_date} + ${(min_profit)}")
 
 
