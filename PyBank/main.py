cd59881180ced63b1332a68df1e6f22b325d9704
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-04-05 20:01:31 -0400

    Adding new files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,53 @@
 import os
 import csv
 
-bank_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+bank_csv = os.path.join("Resources", "budget_data.csv")
 
 #define functions - sole parameter set to wrestler_data
+
+
+
 def print_percentage(bank_data):
     months = int(bank_data[0])
     profits = int(bank_data[1])
     losses = int(bank_data[2])
 
+#calculate totals
     total_months = months
 
     total_amount = profits - losses
 
 
 #total month count
-with open(bank_csv, 'r') as csvfile:
-
+with open(bank_csv, new line='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
     header = next(csvreader)
 
-month_list = [months]
+total_months = len(list())
+
+# lists for csv file values
+months = []
+profit = []
+profit_change = []
+
+#read through the data
+for row in csvreader:
+    months.append = (rows[0])
+    profit.append = (int(row[1]))
+
+for x in range(1,len)
 
 for row in range(len(month_list)):
     month_count == row[0] 
     
 print(f"Month Count: {month_count}")
+
+
+max_change = max(profit_change)
+min_change = min(profit_change)
+
+
+
 
 #seperate profits/losses?
 with open(bank_csv, "w", newline="") as datafile:
@@ -50,6 +71,3 @@ Print(f"Greatest Increase in Profits: {} + {()}")
 Print(f"Greatest Decrease in Profits: {} + {()}")
 
 
-
-      new_length = row[1].split("-")
-    length.append(float(new_length[0]))
